a80ff6890b260e33d4e4d9ed0093c4893d91efb6
mousammm <mxmssdia@gmail.com>
2026-01-21 13:32:24 +0530

    dark

--- config/.config/nvim/lua/plugins/alpha.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'goolord/alpha-nvim',
-  config = function ()
-      require'alpha'.setup(require'alpha.themes.startify'.config)
-      local startify = require'alpha.themes.startify'
-      startify.section.header.val = {}
-      startify.file_icons.enabled = false
-  end
-};
