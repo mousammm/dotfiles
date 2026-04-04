6b3865c433f593ef07c9d3224eca7113b89f4c72
mousammm <mxmssdia@gmail.com>
2026-04-04 18:52:26 +0530

    options remove conenciel

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -35,8 +35,3 @@ vim.o.shiftwidth = 4
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.expandtab = true
-
-vim.opt.conceallevel = 0
-vim.api.nvim_set_hl(0, 'HelpBar', { link = 'Normal' })
-vim.api.nvim_set_hl(0, 'HelpStar', { link = 'Normal' })
-
