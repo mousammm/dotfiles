1404156ccf83dd967bb2b74e524c10864ccb5a0f
mousammm <mxmssdia@gmail.com>
2026-04-14 20:53:58 +0530

    nvim man pages

--- scripts/invim.sh
+++ scripts/invim.sh
@@ -3,6 +3,10 @@
 # required wget tar mv ln
 # remove nvim from pkg manager 
 
+sudo rm /usr/local/bin/nvim                 # binary
+sudo rm /usr/local/share/man/man1/nvim.1    # man
+sudo rm -rf /usr/local/lib/nvim             # src
+
 set -e
 
 NVIM_VERSION="0.12.1"
@@ -11,7 +15,7 @@ NVIM_DIR="nvim-linux-x86_64"
 DOWNLOAD_URL="https://github.com/neovim/neovim/releases/download/v${NVIM_VERSION}/${NVIM_FILE}"
 
 # Download 
-echo "Downloading Neovim ${NVIM_VERSION}..."
+echo "Downloading ${NVIM_DIR} ${NVIM_VERSION}..."
 wget -q --show-progress "$DOWNLOAD_URL"
 
 # Extract
@@ -23,9 +27,13 @@ echo "Installing to /usr/local..."
 sudo mv "$NVIM_DIR" /usr/local/lib/nvim
 sudo ln -sf /usr/local/lib/nvim/bin/nvim /usr/local/bin/nvim
 
+# man pages 
+sudo mkdir -p /usr/local/share/man/man1
+sudo ln -sf /usr/local/lib/nvim/share/man/man1/nvim.1 /usr/local/share/man/man1/nvim.1
+
 # Cleanup
 rm "$NVIM_FILE"
 
-echo "✓ Neovim ${NVIM_VERSION} installed successfully!"
+echo "${NVIM_DIR} ${NVIM_VERSION} installed successfully!"
 echo ""
-nvim --version | head -n1
+nvim --version
