95bb904f8b39d8238f639c3522549d25d70f7ef2
mousammm <mxmssdia@gmail.com>
2026-04-14 20:47:54 +0530

    rg fd man pages

--- scripts/ifd.sh
+++ scripts/ifd.sh
@@ -3,6 +3,10 @@
 # required wget tar mv ln
 # remove fd from pkg manager 
 
+sudo rm /usr/local/bin/fd               # binary
+sudo rm /usr/local/share/man/man1/fd    # man
+sudo rm -rf /usr/local/lib/fd           # src
+
 set -e
 
 FD_VERSION="10.4.2"
@@ -11,7 +15,7 @@ FD_DIR="fd-v${FD_VERSION}-x86_64-unknown-linux-gnu"
 DOWNLOAD_URL="https://github.com/sharkdp/fd/releases/download/v${FD_VERSION}/${FD_FILE}"
 
 # Download 
-echo "Downloading FD ${FD_VERSION}..."
+echo "Downloading ${FD_DIRY} ${FD_VERSION}..."
 wget -q --show-progress "$DOWNLOAD_URL"
 
 # Extract
@@ -23,9 +27,13 @@ echo "Installing to /usr/local..."
 sudo mv "$FD_DIR" /usr/local/lib/fd
 sudo ln -sf /usr/local/lib/fd/fd /usr/local/bin/fd
 
+# man pages 
+sudo mkdir -p /usr/local/share/man/man1
+sudo ln -sf /usr/local/lib/fd/fd.1 /usr/local/share/man/man1/fd.1
+
 # Cleanup
 rm "$FD_FILE"
 
-echo "FD ${FD_VERSION} installed successfully!"
+echo "${FD_DIR} ${FD_VERSION} installed successfully!"
 echo ""
 fd --version
