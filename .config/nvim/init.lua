b165c48938addde36288e2b027e942129090a2e2
mousammm <mxmssdia@gmail.com>
2026-04-19 19:27:06 +0530

    init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,11 +1,6 @@
 require("options")
 require("keymaps")
 
--- require("plugins.vague")
--- require("plugins.telescope")
--- require("plugins.lsp")
--- require("plugins.misc")
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
