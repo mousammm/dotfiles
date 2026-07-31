0d26e1f169c5831951753e686e2a29cd1efa5603
mousammm <mousamssdia@gmail.com>
2026-07-31 11:17:46 +0530

    mod

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -1,19 +1,7 @@
 return {
   {
-    "tpope/vim-fugitive",
     "junegunn/fzf.vim",
     "junegunn/fzf",
-  },
-
-  {
-    "hedyhli/outline.nvim",
-    config = function()
-      vim.keymap.set("n", "<leader>so", "<cmd>topleft Outline<CR>",
-        { desc = "Toggle Outline" })
-
-      require("outline").setup {
-      }
-    end,
   },
 
   {
